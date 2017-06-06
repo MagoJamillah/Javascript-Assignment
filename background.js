2d33cd91a6b8aa112671694e11e274e0ab99d459
Namutebi Jamillah Mago <magojamillah4@gmail.com>
2017-06-06 16:24:48 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
+// Called when the user clicks on a browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Turning ' + tab.url + ' red!');
